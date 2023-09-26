a48b175f7ef194e63773ba02ac3b5b625ea1835b
Sebastian Moreno <56741293+Semorg22@users.noreply.github.com>
2023-09-25 21:54:29 -0500

    Organización
    1. Creación de carpetas
    2. Organización de modulos, css y cambio de nombres

--- home.js
+++ home.js
@@ -1,45 +1,45 @@
-const carousel = document.querySelector('.carousel');
-const prevBtn = document.getElementById('prev-btn');
-const nextBtn = document.getElementById('next-btn');
+const carousel = document.querySelector(".carousel");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
 
 let currentIndex = 0;
 const products = [];
 
 // Cargar productos desde el archivo JSON (reemplaza 'productos.json' con tu archivo JSON)
-fetch('pp.json')
-    .then(response => response.json())
-    .then(data => {
-        products.push(...data);
-        showProducts();
-    })
-    .catch(error => console.error('Error al cargar los productos:', error));
+fetch("home.json")
+  .then((response) => response.json())
+  .then((data) => {
+    products.push(...data);
+    showProducts();
+  })
+  .catch((error) => console.error("Error al cargar los productos:", error));
 
 function showProducts() {
-    carousel.innerHTML = '';
-    for (let i = currentIndex; i < currentIndex + 5; i++) {
-        const productIndex = i % products.length; // Para volver al primer producto al llegar al último
-        const product = products[productIndex];
-        const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
-        productCard.innerHTML = `
+  carousel.innerHTML = "";
+  for (let i = currentIndex; i < currentIndex + 5; i++) {
+    const productIndex = i % products.length; // Para volver al primer producto al llegar al último
+    const product = products[productIndex];
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    productCard.innerHTML = `
             <img src="${product.imagen}" alt="${product.nombre}">
             <h3>${product.nombre}</h3>
             <p>Precio: $${product.precio}</p>
         `;
-        carousel.appendChild(productCard);
-    }
+    carousel.appendChild(productCard);
+  }
 }
 
-prevBtn.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = products.length - 1; // Volver al último producto
-    }
-    showProducts();
+prevBtn.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = products.length - 1; // Volver al último producto
+  }
+  showProducts();
 });
 
-nextBtn.addEventListener('click', () => {
-    currentIndex++;
-    showProducts();
+nextBtn.addEventListener("click", () => {
+  currentIndex++;
+  showProducts();
 });
